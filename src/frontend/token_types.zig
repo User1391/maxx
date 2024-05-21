526571fa75929ae5042ce08212cc8e0fdfef8ab0
User1391 <pye3yh@virginia.edu>
2024-05-21 14:22:14 -0400

    Got started, now I need to do yardwork

---
+++ src/frontend/token_types.zig
@@ -0,0 +1,7 @@
+// shoutout to https://github.com/INDA23PlusPlus/vm/blob/main/src/compiler/types.zig
+const std = @import("std");
+
+// enum of all lang symbols
+pub const Symbol = enum {
+    EXPRESSIONS,
+};
---
+++ src/frontend/tokenizer.zig
@@ -0,0 +1,2 @@
+// lots of insight from https://mitchellh.com/zig/tokenizer
+const std = @import("std");
