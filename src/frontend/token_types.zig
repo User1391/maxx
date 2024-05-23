f97780944846c6035b45a0d5ba3a990ce49e3736
User1391 <pye3yh@virginia.edu>
2024-05-23 15:53:59 -0400

    Forgot to commit these yesterday

--- src/frontend/token_types.zig
+++ src/frontend/token_types.zig
@@ -2,6 +2,79 @@
 const std = @import("std");
 
 // enum of all lang symbols
+// specified in src/docs/syntax.bnf
 pub const Symbol = enum {
-    EXPRESSIONS,
+    STATEMENTS,
+    END_OF_STATEMENTS,
+    STATEMENT,
+
+    VARIABLE_DECLARATION,
+    VARIABLE_MUTATION,
+    FUNCTION_DECLARATION,
+    WHILE_LOOP,
+    FOR_LOOP,
+    IF_STATEMENT,
+    ELIF_STATEMENT,
+    ELSE_STATEMENT,
+    SCOPE,
+    FUNCTION_CALL,
+    RET_STATEMENT,
+
+    EXPRESSION,
+    FUNCTION_PARAMETERS,
+    NO_PARAM,
+    FOR_HEADER,
+    FUNCTION_ARGUMENTS,
+    CALL_CONTINUATION,
+    NO_ARGUMENTS,
+
+    TERM,
+    EXPRESSION_CONTINUATION,
+    END_OF_EXPRESSION,
+    OPERATOR,
+
+    TYPE_DEFINITION,
+    TYPE,
+
+    EQUALS,
+    EQUAL,
+    NOT_EQUAL,
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE,
+    MOD,
+    AND,
+    OR,
+    GT,
+    LT,
+    GEQ,
+    LEQ,
+    OPEN_PARENTHESIS,
+    CLOSED_PARENTHESIS,
+    OPEN_CURLY,
+    CLOSED_CURLY,
+    SEMICOLON,
+    COMMA,
+    RET,
+    INT_TYPE,
+    FN_TYPE,
+    ANY_TYPE,
+    WHILE,
+    FOR,
+    IF,
+    ELSE,
+    ELIF,
+    BREAK,
+
+    IDENTIFIER,
+};
+
+pub const Token = struct {
+    token_type: Symbol, // from above enum
+    content: []const u8,
+    cc_start: u32, // content character
+    cc_end: u32,
+    cln_start: u32, // content line
+    cln_end: u32,
 };
--- src/frontend/tokenizer.zig
+++ src/frontend/tokenizer.zig
@@ -1,2 +1,55 @@
 // lots of insight from https://mitchellh.com/zig/tokenizer
 const std = @import("std");
+const types = @import("token_types.zig");
+
+// set up errors
+const TokenizerError = error{
+    InvalidCharacter,
+};
+
+const Symbol = types.Symbol;
+const Token = types.Token;
+
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
+
+const Self = @This();
+
+allocator: Allocator,
+tokens: ArrayList(Token),
+
+pub fn init(allocator: Allocator) Self {
+    return .{ .allocator = allocator, .tokens = ArrayList(Token.init(allocator)) };
+}
+
+pub fn deinit(self: *Self) void {
+    for (self.tokens.items) |i| {
+        self.allocator.free(i.content);
+    }
+    self.tokens.deinit();
+}
+
+pub fn tokenize(self: *Self, text: []const u8) !void {
+    var ln: u32 = 1;
+    var cn: u32 = 1;
+
+    for (text) |char| {
+        
+    }
+}
+
+
+
+
+test "Tokenize Example Code" {
+    var lexer = Self.init(std.heap.GeneralPurposeAllocator);
+    defer lexer.deinit();
+
+    try lexer.tokenize("varName[int] = 24;");
+
+    try std.testing.expectEqual(lexer.tokens.items.len, 5);
+
+
+
+
+}
